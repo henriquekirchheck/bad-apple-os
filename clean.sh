e273deb5a6958c2a499135306973660b7141da0e
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-07-02 19:37:58 -0300

    Restarting project

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source "$(pwd)/env.sh"
-
-[ -d $ROOTFS_FOLDER ] && rm -r $ROOTFS_FOLDER
-[ -d $BUILD_FOLDER ] && rm -r $BUILD_FOLDER
-[ -d $BIN_FOLDER ] && rm -r $BIN_FOLDER
--- env.sh
+++
@@ -1,7 +0,0 @@
-export BUILD_FOLDER="$(pwd)/build"
-export ROOTFS_FOLDER="$(pwd)/rootfs"
-export SRC_FOLDER="$(pwd)/src"
-export BIN_FOLDER="$ROOTFS_FOLDER/usr/bin"
-export LINUX_VERSION="5.15.51"
-export MUSLLIBC_VERSION="1.2.3"
-export BUSYBOX_VERSION="1.35.0"
