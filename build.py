4a8e04f56ed1514f0f20b84fafb529f1b925e191
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-07-02 23:01:12 -0300

    main build file now has downloadSources

---
+++ build.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+from lib.download import downloadFiles
+
+load_dotenv()
+
+linux_version = os.environ.get('LINUX_VERSION', '5.10.128')
+download_folder = os.path.join(os.getcwd(), 'download')
+
+def downloadSources():
+  sources = [
+    f"https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-{linux_version}.tar.xz"
+  ]
+
+  return [{
+    'path': path,
+    'filename': os.path.basename(path)
+  } for path in downloadFiles(sources, download_folder, True)]
+
+arquive = downloadSources()
+print(arquive)
