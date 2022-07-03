fe6c379e2af93b971fabdd0fe0a1a507a7bee060
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-07-02 21:26:04 -0300

    Added a optional progress bar to download

--- lib/download.py
+++ lib/download.py
@@ -1,6 +1,9 @@
-def downloadFile(url: str, path: str):
-  from requests import get
+def downloadFile(url: str, path: str, progress: bool = False):
   import os
+  import shutil
+
+  from requests import get
+  from tqdm.auto import tqdm
 
   if os.path.isfile(path):
     raise Exception("Path cannot be a File")
@@ -9,12 +12,21 @@ def downloadFile(url: str, path: str):
     os.makedirs(path)
 
   filename = url.split("/")[-1]
-  with get(url) as response:
-    with open(os.path.join(path, filename), 'wb') as file:
-      file.write(response.content)
+
+  if progress:
+    with get(url, stream=True) as res:
+      total_length = int(res.headers.get("Content-Length"))
+
+      with tqdm.wrapattr(res.raw, "read", total=total_length, desc=f"Downloading {filename}") as raw:
+        with open(os.path.join(path, filename), 'wb') as file:
+          shutil.copyfileobj(raw, file)
+  else:
+    with get(url) as response:
+      with open(os.path.join(path, filename), 'wb') as file:
+        file.write(response.content)
 
   return os.path.join(path, filename)
 
 if __name__ == '__main__':
   import os
-  print(downloadFile('https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.52.tar.xz', os.path.join(os.getcwd(), 'test')))
+  print(downloadFile('https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.52.tar.xz', os.path.join(os.getcwd(), 'download-test'), False))
