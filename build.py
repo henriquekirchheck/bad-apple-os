2b0ab8fd8604d2582a735a0f2c3b81ddaae06ecb
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-07-03 21:37:30 -0300

    created createRootFS and updated info.json

--- build.py
+++ build.py
@@ -5,24 +5,36 @@ from lib.download import downloadFile
 from lib.extract import extractTar
 
 with open(os.path.join(os.getcwd(), 'info.json')) as info:
-    data = json.load(info)
+    info = json.load(info)
+
+source = info['source']
+rootfs = info['rootfs']
 
 download_dir = os.path.join(os.getcwd(), 'download')
 build_dir = os.path.join(os.getcwd(), 'build')
+rootfs_dir = os.path.join(os.getcwd(), 'rootfs')
 
 
-def downloadData(data):
-    for package, info in data.items():
-        data[package]["filePath"] = downloadFile(
+def downloadSource(source):
+    for package, info in source.items():
+        source[package]["filePath"] = downloadFile(
             info["url"].replace('[VERSION]', info['version']), download_dir, True)
 
 
-def extractData(data):
-    for package, info in data.items():
-        data[package]["buildDir"] = extractTar(
+def extractSource(source):
+    for package, info in source.items():
+        source[package]["buildDir"] = extractTar(
             info["filePath"], build_dir, True)
 
 
-downloadData(data)
-extractData(data)
-print(data)
+def createRootFS(rootfs):
+    os.mkdir(rootfs_dir)
+    for dir in rootfs['dir']:
+        os.mkdir(os.path.join(rootfs_dir, dir))
+    for symlink in rootfs['symlink']:
+        os.symlink(symlink[0], os.path.join(rootfs_dir, symlink[1]))
+
+
+downloadSource(source)
+extractSource(source)
+createRootFS(rootfs)
